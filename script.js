1210d648980967d5d821125ed27f43e2f9c451f8
cdlms <1210sm7@gmail.com>
2024-08-07 21:42:38 +0900

    동적 로딩

--- script.js
+++ script.js
@@ -5,13 +5,45 @@ Page_game = document.querySelector('#Page_game');
 iframe = document.querySelector('#iframe');
 
 Page_me.addEventListener('click', function() {
-    iframe.src = 'cdlms.html'
+	iframe.src = 'cdlms.html'
 });
 
 Page_dev.addEventListener('click', function() {
-    iframe.src = 'dev.html'
+	iframe.src = 'dev.html'
 });
 
 Page_game.addEventListener('click', function() {
-    iframe.src = 'game.html'
+	iframe.src = 'game.html'
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+	const postList = document.getElementById('post-list');
+	const postContent = document.getElementById('post-content');
+  
+	// 포스트 목록 로드
+	fetch('posts/list.json')
+		.then(response => response.json())
+		.then(posts => {
+			posts.forEach(post => {
+				const link = document.createElement('a');
+				link.href = '#';
+				link.textContent = post.title;
+				link.addEventListener('click', (e) => {
+					e.preventDefault();
+					loadPost(post.file);
+				});
+				postList.appendChild(link);
+			});
+		});
+  
+	// 포스트 로드 함수
+	function loadPost(filename) {
+		fetch(`posts/${filename}`)
+			.then(response => response.text())
+			.then(markdown => {
+				postContent.innerHTML = marked.parse(markdown);
+			});
+	}
+  });
+  
+  
